09292e252df890df8fbb807b2e89569e8a7f8a80
Peter W <techieshark@gmail.com>
2014-06-24 02:39:30 -0700

    improve speed by grabbing quote outside requests

--- web.js
+++ web.js
@@ -4,6 +4,8 @@ var logfmt = require("logfmt");
 var sys = require('sys')
 var exec = require('child_process').exec;
 var Entities = require('html-entities').AllHtmlEntities;
+var http = require('http');
+var decoder = new (require('string_decoder').StringDecoder)('utf-8')
 
 entities = new Entities();
 
@@ -11,15 +13,33 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+var quote = "Calvin: Know what I pray for?  Hobbes: What?  Calvin: The strength to change\n\
+            what I can, the inability to accept what I can't, and the incapacity to tell\n\
+            the difference.  -- Calvin";
+
+function updateQuote() {
+	http.get("http://www.iheartquotes.com/api/v1/random?source=calvin&show_permalink=false&show_source=false", function(res) {
+
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+
+      quote = decoder.write(chunk);
+    });
+
+  });
+};
+
+updateQuote();
+
 app.get('/', function(req, res) {
+  // since Slack just wants text, we need to pass back " and ' instead of html special entities.
+  console.log(quote);
+  console.log('----------');
+  console.log('quote is a ' + typeof quote)
+  res.send(entities.decode(quote));
 
-	function puts(error, stdout, stderr) {
-		// since Slack just wants text, we need to pass back " and ' instead of html special entities. 
-		res.send(entities.decode(stdout));
-	}
-
-  exec('curl "http://www.iheartquotes.com/api/v1/random?source=calvin&show_permalink=false&show_source=false"', puts);
-
+  // grab a new quote for the next request
+  updateQuote();
 });
 
 var port = Number(process.env.PORT || 5000);
